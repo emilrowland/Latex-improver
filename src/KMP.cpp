d734714ff04cd7fd36ed1a3e4aef6fb2a3f723fa
Emil Rowland <rowland.emil@gmail.com>
2017-04-24 10:13:28 +0200

    Fixed KMP

--- src/KMP.cpp
+++ src/KMP.cpp
@@ -2,22 +2,26 @@
 
 KMP::KMP(std::string pattern, int* next, int next_size){
     KMP::KMP_next.assign(next, next + next_size/sizeof(int));
+    KMP::KMP_next.insert(KMP::KMP_next.begin(),0);
     std::copy(pattern.begin(), pattern.end(), std::back_inserter(KMP::KMP_pattern));
 }
 bool KMP::read(char c){
-    int i = KMP::KMP_pos - 1;
-    if(i == -1){
-        i = 0;
-    }
+    int i = KMP::KMP_pos;
     if(c == KMP::KMP_pattern[i]){
-        i += 2;
-        if(i >= KMP::KMP_pattern.size() + 1){
+        i ++;
+        if(i >= KMP::KMP_pattern.size()){
             KMP::KMP_pos = 0;
             return true;
         }
     }
     else{
-        i = KMP::KMP_next[i];
+        std::cout << i << std::endl;
+        while(i > 1){
+            i = KMP::KMP_next[i];
+            std::cout << i << std::endl;
+            KMP::KMP_pos = i;
+            KMP::read(c);
+        }
     }
     KMP::KMP_pos = i;
     return false;
