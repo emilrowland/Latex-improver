7bae599c10ceb793b5a9687b808c1aeb49523031
Emil Rowland <emil.rowland@rowlandindustries.se>
2017-04-26 16:51:50 +0200

    Added test

---
+++ tests/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+../LatexImprover testFile1.tex << EOF
+q
+EOF
+
+echo ""
+
+#compare files
+if ! cmp newFile.tex testFile1CorrectResponse.tex
+then
+    #Error
+    rm newFile.tex
+    return 1
+fi
+rm newFile.tex
